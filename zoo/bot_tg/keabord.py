2d3a368a8c8cb5d5cadd433bc320267cc44247fb
beeteepee <d.a.tishenko@gmail.com>
2024-10-06 13:57:41 +0300

    код без изменений

---
+++ zoo/bot_tg/keabord.py
@@ -0,0 +1,48 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, KeyboardButton
+
+main = InlineKeyboardMarkup(inline_keyboard=[
+    [
+        InlineKeyboardButton(text='Тигр', callback_data='tiger'),
+        InlineKeyboardButton(text='Слон', callback_data='slon')
+    ],
+    [
+        InlineKeyboardButton(text='Бегемот', callback_data='begemot'),
+        InlineKeyboardButton(text='Ленивец', callback_data='leniv'),
+        InlineKeyboardButton(text='Жираф', callback_data='giraffe')
+    ]
+])
+
+
+tiger = InlineKeyboardMarkup(inline_keyboard=[
+    
+    [InlineKeyboardButton(text='Расписание', callback_data='time'),
+    InlineKeyboardButton(text='Место обитания', callback_data='gues')],
+    [InlineKeyboardButton(text='Назад', callback_data='back_menu')]
+])
+
+tiger_1 = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='Назад', callback_data='back_tiger')]
+])
+
+slon = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='Расписание', callback_data='time_slon'),
+    InlineKeyboardButton(text='Место обитания', callback_data='gues_slon')],
+    [InlineKeyboardButton(text='Назад', callback_data='back_menu_slon')]
+])
+
+
+slon_1 = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='Назад', callback_data='back_slon')]
+])
+
+
+buy = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text='Покупка билетов')]
+],
+resize_keyboard=True
+)
+
+type_ticket = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='Только взрослый', callback_data='big'), InlineKeyboardButton(text='Только детский', callback_data='small')],
+    [InlineKeyboardButton(text= 'Взрослый и детский', callback_data='big_and_small')]
+])
